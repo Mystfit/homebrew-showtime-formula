0185d820d418f930f7a847f61e3669cecaf6c278
Byron Mallett <byronmallett@staff-wg402.staff.vuw.ac.nz>
2018-12-10 18:13:31 +1300

    Using czmq/zeromq draft flags

--- Formula/showtime.rb
+++ Formula/showtime.rb
@@ -4,15 +4,23 @@ class Showtime < Formula
   url "https://github.com/Mystfit/Showtime-Cpp/archive/develop.zip"
   version "0.16.1"
   #sha256 "1fa7efa8a5926d59e5861913778c06b634d6f055e3ded0e282d6058350996c75"
+
+  option "with-drafts"
   
   depends_on "cmake" => :build
   depends_on "msgpack"
   depends_on "fmt"
   depends_on "swig"
   depends_on "boost"
-  depends_on "zeromq" => "with-drafts"
-  depends_on "czmq" => "with-drafts"
   depends_on "mystfit/showtime/nlohmann_json"
+  
+  args = []
+  if build.with? "drafts"
+    args << "with-drafts"
+  end
+
+  depends_on "zeromq" => args
+  depends_on "czmq" => args  
 
   def install
     system "cmake", "-DBUILD_DRAFTS=ON", "-DBUILD_STATIC=ON", "-DBUILD_SHARED=ON", "-DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}", *std_cmake_args, "."
